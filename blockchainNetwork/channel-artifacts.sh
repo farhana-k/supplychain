387bca07396729219d9e0675ea38338af8a91947
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2023-05-16 17:30:06 +0530

    update

--- blockchainNetwork/channel-artifacts.sh
+++ blockchainNetwork/channel-artifacts.sh
@@ -1,21 +1,21 @@
 # Genesis Block
 configtxgen -profile RaftOrderer -channelID test-sys-channel -outputBlock ./channel-artifacts/genesis.block
 
-#Channel
+# #Channel
 
-configtxgen -profile common -outputCreateChannelTx ./channel-artifacts/common.tx -channelID common
+# configtxgen -profile common -outputCreateChannelTx ./channel-artifacts/common.tx -channelID common
 
-#Anchor Peer of Org1
+# #Anchor Peer of Org1
 
-configtxgen -profile common -outputAnchorPeersUpdate ./channel-artifacts/org1AnchorPeer.tx -channelID common -asOrg org1MSP
+# configtxgen -profile common -outputAnchorPeersUpdate ./channel-artifacts/org1AnchorPeer.tx -channelID common -asOrg org1MSP
 
-#Anchor peer of Org2
+# #Anchor peer of Org2
 
-configtxgen -profile common -outputAnchorPeersUpdate ./channel-artifacts/org2AnchorPeer.tx -channelID common -asOrg org2MSP
+# configtxgen -profile common -outputAnchorPeersUpdate ./channel-artifacts/org2AnchorPeer.tx -channelID common -asOrg org2MSP
 
-#Anchor peer of Org3
+# #Anchor peer of Org3
 
-configtxgen -profile common -outputAnchorPeersUpdate ./channel-artifacts/org3AnchorPeer.tx -channelID common -asOrg org3MSP
+# configtxgen -profile common -outputAnchorPeersUpdate ./channel-artifacts/org3AnchorPeer.tx -channelID common -asOrg org3MSP
 
 
 ##############################
--- blockchainNetwork/org2/cli-scripts/create-common-approve.sh
+++ blockchainNetwork/org2/cli-scripts/create-common-approve.sh
@@ -10,11 +10,14 @@ echo "***************** queryinstalled Pineapple chaincode ***************"
                     
 echo "***************** ApproveforMyOrg Pineapple chaincode ***************"
                     CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.test.com/users/Admin@org2.test.com/msp CORE_PEER_ADDRESS=peer0.org2.test.com:7051 CORE_PEER_LOCALMSPID="org2MSP" CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.test.com/peers/peer0.org2.test.com/tls/ca.crt 
-                    peer lifecycle chaincode approveformyorg -o orderer1.orderer.test.com:7050 --ordererTLSHostnameOverride orderer1.orderer.test.com --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/orderer/tls/tlsca.orderer.test.com-cert.pem --channelID common --name Pineapple --version 1.0 --package-id $PACKAGE_ID --sequence 1    --signature-policy "OR('org1MSP.peer','org2MSP.peer','org3MSP.peer')"
+                    peer lifecycle chaincode approveformyorg -o orderer1.orderer.test.com:7050 --ordererTLSHostnameOverride orderer1.orderer.test.com --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/orderer/tls/tlsca.orderer.test.com-cert.pem --channelID channela --name Pineapple --version 1.0 --package-id $PACKAGE_ID --sequence 1    --signature-policy "OR('org1MSP.peer','org2MSP.peer','org3MSP.peer')"
                     
 echo "***************** checkCommitReadiness Pineapple chaincode ***************"
                     CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.test.com/users/Admin@org2.test.com/msp CORE_PEER_ADDRESS=peer0.org2.test.com:7051 CORE_PEER_LOCALMSPID="org2MSP" CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.test.com/peers/peer0.org2.test.com/tls/ca.crt 
-                    peer lifecycle chaincode checkcommitreadiness --channelID common --name Pineapple --version 1.0 --sequence 1    --signature-policy "OR('org1MSP.peer','org2MSP.peer','org3MSP.peer')"
+                    peer lifecycle chaincode checkcommitreadiness --channelID channela --name Pineapple --version 1.0 --sequence 1    --signature-policy "OR('org1MSP.peer','org2MSP.peer','org3MSP.peer')"
+
+
+
 
 echo "***************** queryinstalled Jam chaincode ***************"
                     CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.test.com/users/Admin@org2.test.com/msp CORE_PEER_ADDRESS=peer0.org2.test.com:7051 CORE_PEER_LOCALMSPID="org2MSP" CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.test.com/peers/peer0.org2.test.com/tls/ca.crt 
@@ -25,12 +28,10 @@ echo "***************** queryinstalled Jam chaincode ***************"
                     
 echo "***************** ApproveforMyOrg Jam chaincode ***************"
                     CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.test.com/users/Admin@org2.test.com/msp CORE_PEER_ADDRESS=peer0.org2.test.com:7051 CORE_PEER_LOCALMSPID="org2MSP" CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.test.com/peers/peer0.org2.test.com/tls/ca.crt 
-                    peer lifecycle chaincode approveformyorg -o orderer1.orderer.test.com:7050 --ordererTLSHostnameOverride orderer1.orderer.test.com --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/orderer/tls/tlsca.orderer.test.com-cert.pem --channelID common --name Jam --version 1.0 --package-id $PACKAGE_ID --sequence 1    --signature-policy "OR('org1MSP.peer','org2MSP.peer','org3MSP.peer')"
+                    peer lifecycle chaincode approveformyorg -o orderer1.orderer.test.com:7050 --ordererTLSHostnameOverride orderer1.orderer.test.com --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/orderer/tls/tlsca.orderer.test.com-cert.pem --channelID channelb --name Jam --version 1.0 --package-id $PACKAGE_ID --sequence 1    --signature-policy "OR('org1MSP.peer','org2MSP.peer','org3MSP.peer')"
                     
 echo "***************** checkCommitReadiness Jam chaincode ***************"
                     CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.test.com/users/Admin@org2.test.com/msp CORE_PEER_ADDRESS=peer0.org2.test.com:7051 CORE_PEER_LOCALMSPID="org2MSP" CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.test.com/peers/peer0.org2.test.com/tls/ca.crt 
-                    peer lifecycle chaincode checkcommitreadiness --channelID common --name Jam --version 1.0 --sequence 1    --signature-policy "OR('org1MSP.peer','org2MSP.peer','org3MSP.peer')"
+                    peer lifecycle chaincode checkcommitreadiness --channelID channelb --name Jam --version 1.0 --sequence 1    --signature-policy "OR('org1MSP.peer','org2MSP.peer','org3MSP.peer')"
 
 
-                    CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.test.com/users/Admin@org2.test.com/msp CORE_PEER_ADDRESS=peer0.org2.test.com:7051 CORE_PEER_LOCALMSPID="org2MSP" CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.test.com/peers/peer0.org2.test.com/tls/ca.crt 
-                    peer lifecycle chaincode checkcommitreadiness --channelID common --name PurchaseLog --version 1.0 --sequence 1    --signature-policy "OR('org1MSP.peer','org2MSP.peer','org3MSP.peer')"
